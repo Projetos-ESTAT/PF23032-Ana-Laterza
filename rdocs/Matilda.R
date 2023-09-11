2da608a5d078afe7c71d763d355b927e600d182a
Mateus Fernandes Da Silva Lima <20220017@sarah.br>
2023-09-11 14:01:05 -0300

    nuvens e etc.

--- rdocs/Matilda.R
+++ rdocs/Matilda.R
@@ -55,7 +55,55 @@ banco2$TEXTO <- tolower(banco2$TEXTO)
 banco2$TEXTO <- removePunctuation(banco2$TEXTO)
 banco2$TEXTO <- stripWhitespace(banco2$TEXTO)
 banco2$TEXTO <- str_replace_all(banco2$TEXTO, "\\b(\\w+)s\\b", "\\1")
+# criando um corpus para geral 
+lista <- list()
+for (i in 1:nrow(banco2)){
+  lista[[i]] <- banco2[i,3]
+}
+vetor <- unlist(lista)
+docs <- Corpus(VectorSource(vetor))
 
+docs <- docs %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace)
+docs <- tm_map(docs, content_transformer(tolower))
+
+dtm <- TermDocumentMatrix(docs) 
+matrix <- as.matrix(dtm) 
+words <- sort(rowSums(matrix),decreasing=TRUE) 
+df <- data.frame(word = names(words),freq=words)
+
+filtro <- c('que','para','com','isso','tem','por','uma','pra','esse','mais',
+            'dos','das','essa','tá','nas','nem','sem','aos','sobre','aí','pois',
+            'este','esse','dos','ela','pra', 'está', 'mas',"vamos","vai",
+            "estão","nossa","foi","nosso","nos","aqui","ainda","meu","ter",
+            "porque","nossos","são","vão","você","sua","seu","acima","seja",
+            "temos","será","sera","mesmo","pelo","só","so","então","entao",
+            "umas","como","dele","fazendo","galera","alguém","alguem","assim",
+            "número","numero","qualquer","ser","ficar","fazer","ja","já","nossas",
+            "manter","estou","cara","nesse","olha","sair","passar","esses","estas",
+            "dar","pela","esta","apenas","pro","souza","silva","chegar","dessa",
+            "roberto","outra","sendo","estava","gilson","josé","durante","marcus",
+            "marcos","desse","vou","tirar","era","teve","vocês","voces","ontem",
+            "também","tbm","tambem","atraves","através","única","unica","costa",
+            "consegue","as","às","ás","vem","vêm","null","não","ele","la","lá",
+            "sim","boa","estamos","agora","hoje","dia","muito","quem","até",
+            "pode","bom","nome","quando","coisa") # adicionar mais conforme necessidade
+
+#rm(df2,docs,dtm,lista,matrix,filtro,i,vetor,words)
+#gc()
+
+# rode este bloco apenas se desejar filtrar palavras específicas
+df <- df |>
+  filter(!(word %in% filtro)) |> # Aplicando filtros
+  filter(freq >9)
+
+set.seed(150167636) 
+
+wordcloud(words = df$word, freq = df$freq, min.freq = 2,
+          max.words=200, random.order=FALSE, rot.per=0.35, # Ajuste o parâmetro max.words conforme necessidade. O máximo é 200.
+          colors=c("#CA1D1F", "#F55D1C", "#F55751", "#086C75"))
 # criando um corpus (palavra) agrupado por grupo de id
 corpus_list <- split(banco2$TEXTO, banco2$CLASSIFICACAO)
 corpus <- Corpus(VectorSource(corpus_list))
@@ -74,23 +122,6 @@ palavras_frequencia <- text_df %>%
   summarise_all(sum) %>%
   pivot_longer(cols = -grupo, names_to = "palavra", values_to = "frequencia") %>%
   arrange(grupo, desc(frequencia))
-
-filtro <- c('que','para','com','isso','tem','por','uma','pra','esse','mais',
-            'dos','das','essa','tá','nas','nem','sem','aos','sobre','aí','pois',
-            'este','esse','dos','ela','pra', 'está', 'mas',"vamos","vai",
-            "estão","nossa","foi","nosso","nos","aqui","ainda","meu","ter",
-            "porque","nossos","são","vão","você","sua","seu","acima","seja",
-            "temos","será","sera","mesmo","pelo","só","so","então","entao",
-            "umas","como","dele","fazendo","galera","alguém","alguem","assim",
-            "número","numero","qualquer","ser","ficar","fazer","ja","já","nossas",
-            "manter","estou","cara","nesse","olha","sair","passar","esses","estas",
-            "dar","pela","esta","apenas","pro","souza","silva","chegar","dessa",
-            "roberto","outra","sendo","estava","gilson","josé","durante","marcus",
-            "marcos","desse","vou","tirar","era","teve","vocês","voces","ontem",
-            "também","tbm","tambem","atraves","através","única","unica","costa",
-            "consegue","as","às","ás","vem","vêm","null","não","ele","la","lá",
-            "sim","boa","estamos","agora","hoje","dia","muito","quem","até",
-            "pode","bom","nome","quando","coisa")
 
 palavras_frequencia <- palavras_frequencia |> filter(!(palavra %in% filtro))
 
@@ -120,7 +151,8 @@ grupo2 <- grupo2[1:10,] #top10
 grupo3 <- palavras_frequencia %>%
   filter(grupo == '3') %>%
   top_n(20,wt = frequencia)
-grupo3 <- grupo3[-c(2:3,7:8,10,16,17,19,21),] #removendo as stopwords que não foram retiradas
+grupo3 <- grupo3[1:20,]#removendo as stopwords que não foram retiradas
+grupo3 <- grupo3[-c(2,6,9,15,20),]
 wordcloud(words = grupo3$palavra,scale = c(3,.5), freq = grupo3$frequencia,colors = c("#CA1D1F", "#F55D1C", "#F55751", "#086C75")) # tá dando erro essa merda de nuvem
 grupo3 <- grupo3[1:10,]#top10
 
